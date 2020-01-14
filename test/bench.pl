11ee289696825f13caff62107386941fae99c53d
Andrea Telatin <andrea@telatin.com>
2020-01-14 15:49:40 +0000

    Benchmarked

--- test/bench.pl
+++ test/bench.pl
@@ -10,11 +10,16 @@ my $count = 20;
 
 say 'DEINTERLEAVE';
 
-
+my $bigtmp = '/tmp/ilv.fq';
+for (my $i = 1; $i <= 50; $i++) {
+	system(qq (cat "$datadir/interleaved_suffix.fq" >> "$bigtmp") );
+}
 #'interleaved_suffix.fq.gz', 'interleaved_quality_error.fq', 'interleaved_error.fq'
-for my $file ('interleaved_suffix.fq', 'ilv.fq') {
+for my $file ('interleaved_suffix.fq', "$bigtmp") {
  say " - $file";
- my $filepath = "$datadir$file";
+ my $filepath = $file;
+ $filepath = "$datadir$file" if (substr($file, 0, 1) ne '/');
+
  my $perl = qq(perl "$scriptdir/interleafq.pl" -o "$datadir/T_" "$filepath");
  #deinterleave_fastq.sh < interleaved.fastq f.fastq r.fastq [compress]
  my $bash = qq(bash "$scriptdir/deinterleave.sh" < "$filepath" "$datadir/T_1" "$datadir/T_2");
